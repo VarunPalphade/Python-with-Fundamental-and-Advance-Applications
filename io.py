c239ff8ac49e8c27bb04090c3cbd816237c5afa1
VarunPalphade <palphadevarun@gmail.com>
2024-08-17 17:57:46 +0530

    added first file

---
+++ io.py
@@ -0,0 +1,14 @@
+#to accept the input from user 
+
+print("enter your name")
+
+name = input()
+
+print("Hello " +name)
+
+
+name1 = input("enter your name again ")
+
+print("Hello " +name1)
+
+print(type(name))
